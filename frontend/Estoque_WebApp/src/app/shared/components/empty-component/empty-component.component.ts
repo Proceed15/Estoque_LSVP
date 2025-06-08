4c628a7fd73256cf05120e0b34174ae74221a345
JRLSF <jlisboa600@gmail.com>
2025-06-08 00:03:25 -0300

    Cre\ted Empty Component
    Jose RLSF

---
+++ frontend/Estoque_WebApp/src/app/shared/components/empty-component/empty-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-component',
+  imports: [],
+  templateUrl: './empty-component.component.html',
+  styleUrl: './empty-component.component.css'
+})
+export class EmptyComponentComponent {
+
+}
