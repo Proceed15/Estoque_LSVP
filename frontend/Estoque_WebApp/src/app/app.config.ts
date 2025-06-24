cc8f09d6b99df37af544709af7dab5ad6d292c95
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-06-24 14:28:32 -0300

    feat:fix_app.config
    app.config fixed
    
    Kaique A Mesquita

--- frontend/Estoque_WebApp/src/app/app.config.ts
+++ frontend/Estoque_WebApp/src/app/app.config.ts
@@ -1,25 +1,14 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient} from '@angular/common/http';
+
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-    new TranslateHttpLoader(http, './i18n/', '.json');
-
 export const appConfig: ApplicationConfig = {
-    providers: [
+  providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
-    importProvidersFrom([TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
-      },
-    })])
-  ],
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
 };
