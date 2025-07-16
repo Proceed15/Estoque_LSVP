8250ee2056c0a4b8c76fcbd37161200cd73687fa
JRLSF <jlisboa600@gmail.com>
2025-07-15 22:14:48 -0300

    Product Model Update

--- frontend/Estoque_WebApp/src/app/shared/models/product.ts
+++ frontend/Estoque_WebApp/src/app/shared/models/product.ts
@@ -1,2 +1,10 @@
 export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    categoryId: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
