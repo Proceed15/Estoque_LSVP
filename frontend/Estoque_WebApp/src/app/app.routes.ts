db545f35061536f2e9f95b5923e72cd6bbf96a11
JRLSF <jlisboa600@gmail.com>
2025-10-22 01:12:22 -0300

    Saved Changes

--- frontend/Estoque_WebApp/src/app/app.routes.ts
+++ frontend/Estoque_WebApp/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ManageLayoutComponent } from './shared/layouts/manage-layout/manage-layout.component';
 import { authGuard } from './core/guards/auth.guard';
-import { loginGuard } from './core/guards/login.guard';
+//import { loginGuard } from './core/guards/login.guard';
 import { UsersViewComponent } from './pages/user/users-view/users-view.component';
 import { ManageViewComponent } from './pages/manage-view/manage-view.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
